d81e75ca8334da7f6c8ef995ee0c10215bb3a44e
mohammed mashrur rafique mia <mohammed.mia@ibm.com>
2022-05-24 16:15:22 +0100

    Add certification generation script

---
+++ env.sh
@@ -0,0 +1,10 @@
+CONTAINER_CLI=docker
+
+PREFIX=ibm
+COMMON_NAME=*.eu-gb.containers.appdomain.cloud
+SAN_DNS="DNS:localhost,DNS:*.itzroks-5500059ths-unonwt-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud"
+
+ORGANISATION=IBM
+COUNTRY=GB
+LOCALITY=London
+STATE=London
